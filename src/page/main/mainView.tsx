7704b6479b110e9a61b22964202b96c30da6eb29
liuxian496 <liuxian494@163.com>
2023-12-30 10:30:00 +0800

    feat: 添加音量调节功能，支持中英文切换

--- src/page/main/mainView.tsx
+++ src/page/main/mainView.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react';
 import './mainView.scss';
 
 import {
+  LittenCheckedChangeEvent,
+  LittenNumberChangeEvent,
+} from 'litten/build/types/components/control/control.types';
+
+import {
   Button,
   Switch,
   Form,
@@ -11,12 +16,15 @@ import {
   TextField,
   useForm,
   StackPanel,
+  Slider,
+  RadioGroup,
+  Radio,
+  Placement,
 } from 'litten';
 
 import { BarrageSetting } from 'page/page.types';
-
-const startSpeaking = '开始朗读';
-const stopSpeaking = '停止朗读';
+import { Local } from 'global/enum';
+import { getLexicon } from 'global/i18n';
 
 export default function MainView() {
   const mainForm = useForm<BarrageSetting>();
@@ -25,7 +33,18 @@ export default function MainView() {
 
   const [connect, setConnect] = useState(false);
 
-  const [speechLabel, setSpeechLabel] = useState(startSpeaking);
+  const [speechLabel, setSpeechLabel] = useState('');
+  const [speech, setSpeech] = useState(true);
+
+  const [i18N, setI18N] = useState(getLexicon(Local.zhCN));
+
+  useEffect(() => {
+    if (speech === true) {
+      setSpeechLabel(i18N.startSpeaking);
+    } else {
+      setSpeechLabel(i18N.stopSpeaking);
+    }
+  }, [i18N, speech]);
 
   const handleRoomIdTextFieldChange = (e: any) => {
     const { value } = e;
@@ -35,8 +54,6 @@ export default function MainView() {
       setLoading(false);
     }
   };
-
-  useEffect(() => {}, [loading]);
 
   const handleConnectBtuClick = () => {
     const values = mainForm.getValues();
@@ -49,36 +66,80 @@ export default function MainView() {
     setConnect(false);
   };
 
-  const handleSpeechChange = (e: any) => {
-    if (e.checked === true) {
-      setSpeechLabel(startSpeaking);
-    } else {
-      setSpeechLabel(stopSpeaking);
+  const handleSpeechChange = (e: LittenCheckedChangeEvent) => {
+    const { checked } = e;
+
+    if (checked != null) {
+      setSpeech(checked);
+      window.electron?.changeSpeech(checked);
     }
-    window.electron?.changeSpeech(e.checked);
+  };
+
+  const handleVolumeChange = (e: LittenNumberChangeEvent) => {
+    const { value } = e;
+    if (value !== undefined) {
+      window.electron?.changeVolume(value);
+    }
+  };
+
+  const handleLanguageRadioGroupChange = (e: LittenCheckedChangeEvent) => {
+    const local = e.value as Local;
+    if (local) {
+      setI18N(getLexicon(local));
+    }
   };
 
   return (
     <>
       <Form formRef={mainForm}>
         <StackPanel direction="column" alignItems="flex-start">
-          <FormLabel label="房间号:">
+          {/* 房间号 */}
+          <FormLabel label={i18N.roomId}>
             <FormControl valuePath="roomId">
               <TextField
                 style={{ marginLeft: '10px' }}
-                placeholder="请输入房间号"
+                defaultValue={8638358}
+                placeholder={i18N.roomId_placeholder}
                 onChange={handleRoomIdTextFieldChange}
               />
             </FormControl>
           </FormLabel>
+
+          {/* 语音播报 */}
           <FormLabel label={speechLabel}>
             <FormControl valuePath="speech">
-              <Switch
-                style={{ marginLeft: '10px' }}
-                defaultChecked
-                onChange={handleSpeechChange}
-              />
+              <Switch defaultChecked onChange={handleSpeechChange} />
             </FormControl>
+          </FormLabel>
+
+          {/* 音量 */}
+          <FormLabel label={i18N.volume} style={{ marginBottom: 10 }}>
+            <StackPanel style={{ width: 200, marginLeft: 10 }}>
+              <FormControl valuePath="volume">
+                <Slider defaultValue={35} onChange={handleVolumeChange} />
+              </FormControl>
+            </StackPanel>
+          </FormLabel>
+
+          {/* 语言切换 */}
+          <FormLabel
+            label={i18N.language}
+            style={{ borderTop: '1px solid #ababab' }}
+          >
+            <RadioGroup
+              defaultValue={Local.zhCN}
+              name="language"
+              onChange={handleLanguageRadioGroupChange}
+            >
+              <StackPanel direction="column" alignItems="flex-start">
+                <FormLabel label="中文简体" labelPlacement={Placement.right}>
+                  <Radio value={Local.zhCN} name="language" />
+                </FormLabel>
+                <FormLabel label="English(US)" labelPlacement={Placement.right}>
+                  <Radio value={Local.enUS} name="language" />
+                </FormLabel>
+              </StackPanel>
+            </RadioGroup>
           </FormLabel>
         </StackPanel>
       </Form>
@@ -89,7 +150,7 @@ export default function MainView() {
             loading={loading}
             onClick={handleConnectBtuClick}
           >
-            连接
+            {i18N.connect}
           </Button>
         )}
         {connect === true && (
@@ -98,7 +159,7 @@ export default function MainView() {
             loading={loading}
             onClick={handleDisconnectBtuClick}
           >
-            断开连接
+            {i18N.disconnect}
           </Button>
         )}
       </div>
